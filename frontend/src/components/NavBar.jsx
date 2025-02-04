1feed6562ff74210ec131549c35fcfe01f678ec1
Richard Li <richard.li2004@hotmail.com>
2025-02-05 05:51:00 +1100

    create navbar and install mantine ui library

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,77 @@
+import { Link } from "react-router-dom";
+import tt_bat from "../images/tt_bat.svg";
+import { useState } from "react";
+import { Button } from "@mantine/core";
+import Classes from "../css/NavBar.module.css";
+
 function NavBar() {
-  return <div>Hello World;</div>;
+  const [currentPage, setCurrentPage] = useState("home");
+
+  function toggleFocus(focus) {
+    setCurrentPage(focus);
+  }
+
+  return (
+    <div className={Classes.navcontainer}>
+      <div className={Classes.navleft}>
+        <div className={Classes.imgcontainer}>
+          <img src={tt_bat} alt="table tennis bat" className={Classes.navimg} />
+        </div>
+
+        <p className={Classes.navtitle}>Richard TT</p>
+      </div>
+      <div className={Classes.navright}>
+        <Button
+          className={currentPage === "home" ? Classes.focused : ""}
+          variant="subtle"
+          radius="xl"
+          onClick={() => {
+            toggleFocus("home");
+          }}
+        >
+          <Link className={Classes.navlink} to="/">
+            Home
+          </Link>
+        </Button>
+        <Button
+          className={currentPage === "about" ? Classes.focused : ""}
+          variant="subtle"
+          radius="xl"
+          onClick={() => {
+            toggleFocus("about");
+          }}
+        >
+          <Link className={Classes.navlink} to="/about">
+            About
+          </Link>
+        </Button>
+        <Button
+          className={currentPage === "lessons" ? Classes.focused : ""}
+          variant="subtle"
+          radius="xl"
+          onClick={() => {
+            toggleFocus("lessons");
+          }}
+        >
+          <Link className={Classes.navlink} to="/lessons">
+            Lessons
+          </Link>
+        </Button>
+        <Button
+          className={currentPage === "more" ? Classes.focused : ""}
+          variant="subtle"
+          radius="xl"
+          onClick={() => {
+            toggleFocus("more");
+          }}
+        >
+          <Link className={Classes.navlink} to="/more">
+            More Info
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;

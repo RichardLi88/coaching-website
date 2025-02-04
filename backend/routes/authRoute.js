1418430f0644131915e6d46caaeb6ebed5f5dac2
Richard Li <richard.li2004@hotmail.com>
2025-02-04 17:01:42 +1100

    add logout route

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { login, signUp } from "../controllers/authController.js";
+import { login, logout, signUp } from "../controllers/authController.js";
 const authRouter = express.Router();
 
 authRouter.post("/signup", signUp);
 authRouter.post("/login", login);
-//authRouter.delete("/delete");
+authRouter.post("/logout", logout);
 
 export default authRouter;

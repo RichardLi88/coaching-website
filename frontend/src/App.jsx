34f0cad17cc299fbdf687574f78c11bbe2caf281
Richard Li <richard.li2004@hotmail.com>
2025-02-05 12:41:10 +1100

    change css files to module css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,21 @@ import Lessons from "./pages/Lessons";
 import NavBar from "./components/NavBar";
 import { Routes, Route } from "react-router-dom";
 import MoreInfo from "./pages/MoreInfo";
+import "@mantine/core/styles.css";
+import { AppShell, MantineProvider } from "@mantine/core";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/lessons" element={<Lessons />} />
-        <Route path="/more" element={<MoreInfo />} />
-      </Routes>
+      <MantineProvider>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/lessons" element={<Lessons />} />
+          <Route path="/more" element={<MoreInfo />} />
+        </Routes>
+      </MantineProvider>
     </>
   );
 }

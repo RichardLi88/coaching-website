4cd03af6cc3c6055b4c0f3b2c55cbf41bbfe3b55
Richard Li <richard.li2004@hotmail.com>
2025-02-25 22:20:47 +1100

    change credentials again

--- frontend/src/utility/fetchAuthentication.js
+++ frontend/src/utility/fetchAuthentication.js
@@ -26,7 +26,7 @@ export async function login(data) {
       "https://webdev-2kdh.onrender.com/api/user/login",
       {
         method: "POST",
-        credentials: include,
+        credentials: "include",
         headers: {
           "Content-Type": "application/json",
         },
